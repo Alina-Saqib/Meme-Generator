65980a81f582ffb17efb4bf71e5ae7366939eb73
Alina-Saqib <alina.saqib500@gmail.com>
2024-01-16 10:31:52 +0500

    whole project

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='header'>
+        <img src='/assets/Logo.png'></img>
+
+        <h3 >React Course- Project 3</h3>
+
+      
+    </header>
+  )
+}
